874eaed7b6ea9c668b3ebca140c6fc64472e2405
Meesam <meesam.engineer@gmail.com>
2023-11-28 13:18:58 +0530

    Completed CRUD operations on Category with notification

--- BulkyStoreApp/Controllers/CategoryController.cs
+++ BulkyStoreApp/Controllers/CategoryController.cs
@@ -30,8 +30,71 @@ public class CategoryController : Controller
         {
             _dbContext.Categories.Add(category);
             _dbContext.SaveChanges();
+            TempData["success"] = "Category created successfully";
             return RedirectToAction("Index");
         }
         return View();
     }
+
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id ==0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        
+        return View(category);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category category)
+    {
+        if (ModelState.IsValid)
+        {
+            _dbContext.Categories.Update(category);
+            _dbContext.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+
+        return View(category);
+    }
+
+    [HttpPost, ActionName("Delete")]
+   
+    public IActionResult DeletePOST(int? id)
+    {
+        Category? category = _dbContext.Categories.FirstOrDefault(c => c.Id == id);
+        if (category == null)
+        {
+            return NotFound();
+        }
+        _dbContext.Categories.Remove(category);
+        _dbContext.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
+
+
 }
